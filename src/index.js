d80025d009c2bf633286dae94041ac2998e94e6b
Valyushka2023 <vvpto@ukr.net>
2024-09-01 23:00:22 +0300

    nodejs-hw-mongodb
    ch001

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { setupServer } from "../server.js";
+import { initMongoConnection } from "./db/initMongoConnection.js";
+
+const bootstrap = async () => {
+  try {
+    // Очікуємо завершення підключення до бази даних
+    await initMongoConnection();
+    console.log("MongoDB connection established");
+
+    // Запускаємо сервер тільки після успішного підключення
+    setupServer();
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+bootstrap();
